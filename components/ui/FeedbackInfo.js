5b736d3920ec9254c2ad7b63f5cba72e8635670c
joshanaba <anabajoshchi@gmail.com>
2023-12-19 13:31:26 +0100

    added fixes to feedback component

--- components/ui/FeedbackInfo.js
+++ components/ui/FeedbackInfo.js
@@ -1,11 +1,11 @@
 import styles from "@/assets/styles/form.module.css";
 import { NoteIcon, SuccessIcon, WarningIcon } from "@/icons/icon";
 
-const FeedbackInfo = ({ message, styleProps, center, type = "error" }) => {
+const FeedbackInfo = ({ message, styleProps, center, type = "error", border=false }) => {
   return (
     <>
       <div
-        className={`${styles.feedback_info_ctn} ${styles[type]} ${
+        className={`${styles.feedback_info_ctn} ${border ? styles.border : ''} ${styles[type]} ${
           center ? styles.center : ""
         }`}
         style={styleProps?.ctn}
